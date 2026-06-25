4e2fbcafc58bf97600a45c0c03d6280c5991e4b7
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-06-25 17:03:35 +0530

    Time: 0 ms (100%), Space: 9.9 MB (77.93%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while(i < j){
+            if(!isalnum(s[i])){
+                i++;
+                continue;
+            }
+            if(!isalnum(s[j])){
+                j--;
+                continue;
+            }
+            if(tolower(s[i]) != tolower(s[j])){
+                return 0;
+            }
+            i++;
+            j--;
+        }
+        return 1;
+    }
+};
