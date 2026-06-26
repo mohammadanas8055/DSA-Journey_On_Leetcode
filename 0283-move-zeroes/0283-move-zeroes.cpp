08aca846343ee6cdb775981a9e33d5aca3cbd9df
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-06-26 12:36:05 +0530

    Update 0283-move-zeroes.cpp

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -12,3 +12,17 @@ public:
         }
     }
 };
+
+/*
+
+O(n) Time Complexity and O(1) Space complexity
+
+Non-zeros remain relative order, j starts from 0 so to ensure if 1st element is also non-zero
+
+Slow-pointer -> next non-zero placement
+Fast-pointr -> scans non-zeros
+jab non-zero ko laga diya, to i++ kar diya
+
+piche se nahi shuru kiya nahi to relative order of non-zeros kharab ho jata
+
+*/
