d5b9ef7dad33272282da026939de5e33685a0c22
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-06-30 18:22:02 +0530

    Time: 0 ms (100%), Space: 22.7 MB (52.71%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -5,7 +5,7 @@ public:
         for(int j = 1; j < nums.size(); j++){ // agla element jo same na ho wo dhoondhna hai, isliye j = 1 se array ke end tak traverse karna hai
             if(nums[i] != nums[j]){ 
                 i++;
-                nums[i] = nums[j]; // agar mil gaya, to us unique element ko le aana hai, apne current wale element ke just baad(isliye i++)
+                nums[i] = nums[j]; // agar mil gaya, to jitne same the(apne current wale element se), unko 1 position se aage shift karte hue us unique element ko le aana hai, apne current wale element ke just baad(isliye i++)
             }// phir loop chalne laga next element ke liye, aur ab us next element se different element dhoodhna hai
             // i ko constant rakhna hai tab tak jab tak koi different element na mil jaye, jaise hi mila koi different element, use aage le aana hai i = i + 1 ki jagah pe
         }
