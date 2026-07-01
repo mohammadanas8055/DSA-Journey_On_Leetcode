627a3b06ffa98b880f5b0910d3ce17633d09cf0d
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-01 17:21:17 +0530

    Time: 0 ms (100%), Space: 50.1 MB (67.7%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -20,6 +20,12 @@ public:
 
 /*
 
+Time complexity -> O(n)
+Space complexity -> O(1)
+
 Consecutive ones ko add kiya, jaise hi 0 mila to usko break kar diya, agar usse zyada kahin 1 mil gaye to us pichle wale count ko replace kar diya
+
+Could also write 
+maxCount = max(count, maxCount);
 
 */
