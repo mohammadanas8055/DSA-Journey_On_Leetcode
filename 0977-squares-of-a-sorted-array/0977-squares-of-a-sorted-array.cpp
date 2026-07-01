1bba05695e307561699d636f6f0ea52fbd06686a
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-01 15:05:37 +0530

    Time: 0 ms (100%), Space: 30.4 MB (31.19%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> result(n);
-        int i = 0; 
+        vector<int> result(nums.size());
+        for(int k = 0; k < nums.size(); k++){
+            nums[k] = nums[k] * nums[k];
+        }
+        int i = 0;
         int j = nums.size() - 1;
         int pos = nums.size() - 1;
         while(i <= j){
-            int leftSquare = nums[i] * nums[i];
-            int rightSquare = nums[j] * nums[j];
-            if(leftSquare > rightSquare){
-                result[pos] = leftSquare;
+            if(nums[i] > nums[j]){
+                result[pos] = nums[i];
                 i++;
             }
             else{
-                result[pos] = rightSquare;
+                result[pos] = nums[j];
                 j--;
             }
             pos--;
@@ -22,22 +22,3 @@ public:
         return result;
     }
 };
-
-/*
-
-Pattern: Two-pointer from opposite ends with auxiliary array
-This pattern appears in:
-
-Squares of Sorted Array ✅ 
-Merge Two Sorted Arrays
-Container with Most Water (variation)
-Many sorted array merge problems
-
-new array banayenge -> Space Complexity O(n)
-but time complexity is reduced from O(nlogn) to O(n) (By sorting method) -> (Squaring first, then sorting)
-but we needed to use the fact that the array IS ALREADY sorted 
-that's why we used two-pointers from opposite ends, because largest values will be at either ends(after squaring)
-so square the extreme elements, compare them, then store them, from BACK to FORTH in a new array
-move the pointers and the position (in the new array) accordingly
-
-*/
