77b2b53e4d7b37fe7fdc44080166407517c269b5
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-02 18:49:33 +0530

    Time: 0 ms (100%), Space: 35.8 MB (56.03%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -18,3 +18,21 @@ public:
         return -1;
     }
 };
+
+/*
+
+i++ or j-- or both belongs to problems like:
+sorted array pair, reverse string, merge sorted array, sort by parity, squares of sorted array
+
+Here there aren't two pointers moving toward each other. This problem is like:
+* Stand at each index and ask: Is the sum on my left equal to the sum on my right?
+That's the required index
+Only one pointer moving from left to right
+
+so first find totalSum , then leftSum ko 0 maan ke , har index pe, rightSum nikaal lenge(total - left - number at index i itself)
+agar equal hue, left aur right sum , to wahi index return kar dena hai
+agar nahi hue, to index ko badhana hi hai, aur us number ko leftSum me store bhi karna hai, jisse aage badhe hain
+AND this should be done AFTER checking, because nums[i] itself should not be included in leftSum
+It becomes left side only for the next index
+
+*/
