23c09094517978e421eabb853a02a01fa71c8ebd
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-02 14:32:31 +0530

    Time: 0 ms (100%), Space: 97.4 MB (24.83%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minimum = prices[0];
-        int profitIfSellToday = 0;
+        int minimum = prices[0]; // ek ek minimum ke liye, baar baar profit dekho, agar maximum profit hai , to wo answer hoga
         int maxProfit = 0;
         for(int i = 0; i < prices.size(); i++){
-            profitIfSellToday = prices[i] - minimum;
+            int profitIfSellToday = prices[i] - minimum; // us day pe sell karne pe profit -> SP - CP(current minimum)
             if(profitIfSellToday > maxProfit){
                 maxProfit = profitIfSellToday;
             }
             if(minimum > prices[i]){
                 minimum = prices[i];
-            }
+            }// minimum update karne ke liye , kyunki maximum profit = maximum SP - minimum CP
         }
         return maxProfit;
     }
 };
+
+/*
+
+Time complexity -> O(n)
+Space complexity -> O(1)
+
+"Single pass + track running state"
+
+This is one of the MOST IMPORTANT patterns in DSA.
+
+Used in:
+
+Best Time to Buy/Sell Stock (today)
+Maximum subarray sum (Kadane's algorithm)
+Find max/min in array
+Many "find optimal X" problems
+
+*/
