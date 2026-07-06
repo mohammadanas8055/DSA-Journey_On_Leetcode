1d713419e7da8caede6e429bc3028c9a60f5c81a
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-06 20:13:43 +0530

    Time: 0 ms (100%), Space: 13.6 MB (25.68%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -1,21 +1,52 @@
+// class Solution {
+// public:
+//     int maxProduct(vector<int>& nums) {
+//         int maxProduct = 1;
+//         int product;
+//         int maxIindex;
+//         int maxJindex;
+//         for(int i = 0; i < nums.size() - 1; i++){
+//             for(int j = i + 1; j < nums.size(); j++){
+//                 product = nums[i] * nums[j];
+//                 if(product >= maxProduct){
+//                     maxProduct = product;
+//                     maxIindex = i;
+//                     maxJindex = j;
+//                 }
+//             }
+//         }
+//         int maximum = (nums[maxIindex] - 1) * (nums[maxJindex] - 1);
+//         return maximum;
+//     }
+// };
+
+// TC -> O(n^2) because checking every pair
+
+/*
+
+Now problem is asking for (nums[i] - 1) * (nums[j] - 1) to be maximum
+Since nums[i] >= 1, best pair would be "Largest two numbers"
+
+So we need to find out Largest and second largest numbers using one-pass tracking
+
+
+
+*/
+
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int maxProduct = 1;
-        int product;
-        int maxIindex;
-        int maxJindex;
-        for(int i = 0; i < nums.size() - 1; i++){
-            for(int j = i + 1; j < nums.size(); j++){
-                product = nums[i] * nums[j];
-                if(product >= maxProduct){
-                    maxProduct = product;
-                    maxIindex = i;
-                    maxJindex = j;
-                }
+        int largest = nums[0];
+        int secondLargest = nums[1];
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] > largest){
+                secondLargest = largest;
+                largest = nums[i];
+            }
+            else if(nums[i] > secondLargest){
+                secondLargest = nums[i];
             }
         }
-        int maximum = (nums[maxIindex] - 1) * (nums[maxJindex] - 1);
-        return maximum;
+        return (largest - 1) * (secondLargest - 1);
     }
 };
