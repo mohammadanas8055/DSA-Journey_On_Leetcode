471127c4dd66056cb0d4163acf78a626803c1b08
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-06 19:13:02 +0530

    Time: 0 ms (100%), Space: 12.7 MB (6.17%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.cpp
+++ 0832-flipping-an-image/0832-flipping-an-image.cpp
@@ -4,17 +4,21 @@ public:
         for(int i = 0; i < image.size(); i++){
             int left = 0; 
             int right = image[i].size() - 1;
-            while(left < right){
-                int temp = image[i][left];
-                image[i][left] = image[i][right];
+            while(left <= right){
+                int temp = 1 - image[i][left];
+                image[i][left] = 1 - image[i][right];
                 image[i][right] = temp;
                 left++;
                 right--;
-            }
-            for(int j = 0; j < image[i].size(); j++){
-                image[i][j] = 1 - image[i][j];
             }
         }
         return image;
     }
 };
+
+/*
+
+TC -> O(n^2) or O(rows*cols) | SC -> O(1)
+
+
+*/
