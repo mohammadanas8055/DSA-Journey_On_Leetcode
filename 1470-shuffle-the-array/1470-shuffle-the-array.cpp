87370976a03d47f888c1a21540368ff3b1bc9893
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-07 20:25:58 +0530

    Time: 4 ms (65.72%), Space: 13.4 MB (52.09%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> result(nums.size(), 0);
+        vector<int> result(nums.size()); // or write 2 * n
         int position = 0;
         for(int i = 0; i < n; i++){
             result[position] = nums[i];
@@ -9,6 +9,15 @@ public:
             result[position] = nums[n + i];
             position++;
         }
+        // There are n pairs and i am placing two elements(one pair) for each iteration
+        // That's why loop n times(n pairs getting placed)
         return result;
     }
 };
+
+/*
+
+TC -> O(n) | SC -> O(n)
+
+*/
+
