c9f60cc087198a83feb890221362991bb15fb60f
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-07 21:31:52 +0530

    Time: 0 ms (100%), Space: 20.5 MB (62.8%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -2,12 +2,15 @@ class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
         vector<int> ans(nums.size());
-        int pos = 0;
         for(int i = 0; i < nums.size(); i++){
-            int temp = nums[i];
-            ans[pos] = nums[temp];
-            pos++;
+            ans[i] = nums[nums[i]];
         }
         return ans;
     }
 };
+
+/*
+
+TC -> O(n) | SC -> O(n)
+
+*/
