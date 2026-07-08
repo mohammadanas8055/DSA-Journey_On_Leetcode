19a20661a42938e5525ee1e9a28975d822e37ceb
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-08 14:26:40 +0530

    Time: 0 ms (100%), Space: 12.3 MB (94.38%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -1,15 +1,43 @@
-class Solution {
+// class Solution {
+// public:
+//     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+//         vector<bool> maxCandies(candies.size(), true);
+//         for(int i = 0; i < candies.size(); i++){
+//             int temp = candies[i] + extraCandies;
+//             for(int j = 0; j < candies.size(); j++){
+//                 if(temp < candies[j]){
+//                     maxCandies[i] = false;
+//                 }
+//             }
+//         }
+//         return maxCandies;
+//     }
+// };
+
+// This is O(n^2) TC
+// For every kid, we are searching whether he has more candies than the maximum one
+// Instead, find the maximum one and then compare each kid with that maximum value
+
+class Solution{
 public:
-    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-        vector<bool> maxCandies(candies.size(), true);
-        for(int i = 0; i < candies.size(); i++){
-            int temp = candies[i] + extraCandies;
-            for(int j = 0; j < candies.size(); j++){
-                if(temp < candies[j]){
-                    maxCandies[i] = false;
-                }
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies){
+        int maximum = candies[0];
+        for(int i = 1; i < candies.size(); i++){
+            if(candies[i] > maximum){
+                maximum = candies[i];
             }
         }
-        return maxCandies;
+        vector<bool> result;
+        for(int i = 0; i < candies.size(); i++){
+            if(candies[i] + extraCandies >= maximum){
+                result.push_back(true);
+            }
+            else{
+                result.push_back(false);
+            }
+        }
+        return result;
     }
 };
+
+// TC -> O(n) + O(n) = O(n) | SC -> O(n)
