b9606a7900316829b2a0af9e6b80b70a73f00c9d
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-09 16:24:26 +0530

    Time: 0 ms (100%), Space: 9.8 MB (23.88%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
         int pairs = 0;
-        int arr[101] = {0};
-        int frequency = 0;
+        int frequency[101] = {0};
         for(int i = 0; i < nums.size(); i++){
-            pairs = pairs + arr[nums[i]];
-            arr[nums[i]]++;
+            pairs = pairs + frequency[nums[i]];
+            frequency[nums[i]]++;
         }
         return pairs;
     }
@@ -17,8 +16,16 @@ public:
 Stand on a number and see how many times the same number appeared BEFORE that position
 
 arr[x] stores: how many times number x has appeared before current index
-Inputs were 1 to 100 , so made 100 
+Inputs were 1 to 100 , so made 101 boxes(0 to 100 indexed) for counting how many times a number has appeared before
 
+If current number appeared before k times, then k new pairs
+Phir current number ka count badha diya for fiture elements
 
+TC -> O(n) | SC -> O(1) because freq[101] is fixed size
+
+Pattern:
+frequency count with fixed-size array
+
+Here we used frequency while scanning, instead of checking at the end(in case of Valid Anagram)
 
 */
