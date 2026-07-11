e0807da38ba1424a8786c9e2336c688f4e03c237
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-11 19:34:19 +0530

    Time: 0 ms (100%), Space: 8.4 MB (85.51%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,30 +1,72 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char> s1;
-        stack<char> t1;
-        for(int i = 0; i < s.length(); i++){
-            if(!s1.empty() && s[i] == '#'){
-                s1.pop();
+        // stack<char> s1;
+        // stack<char> t1;
+        // for(int i = 0; i < s.length(); i++){
+        //     if(!s1.empty() && s[i] == '#'){
+        //         s1.pop();
+        //     }
+        //     else if(s[i] == '#'){
+        //         continue;
+        //     }
+        //     else{
+        //         s1.push(s[i]);
+        //     }
+        // }
+        // for(int i = 0; i < t.length(); i++){
+        //     if(!t1.empty() && t[i] == '#'){
+        //         t1.pop();
+        //     }
+        //     else if(t[i] == '#'){
+        //         continue;
+        //     }
+        //     else{
+        //         t1.push(t[i]);
+        //     }
+        // }
+        // return s1 == t1;
+
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+        int skipS = 0;
+        int skipT = 0;
+        while(i >= 0 || j >= 0){
+            while(i >= 0){
+                if(s[i] == '#'){     
+                    skipS++;
+                    i--;
+                }
+                else if(skipS > 0){
+                    skipS--;
+                    i--;
+                }
+                else{
+                    break;
+                }
             }
-            else if(s[i] == '#'){
-                continue;
+            while(j >= 0){
+                if(t[j] == '#'){
+                    skipT++;
+                    j--;
+                }
+                else if(skipT > 0){
+                    skipT--;
+                    j--;
+                }
+                else{
+                    break;
+                }
             }
-            else{
-                s1.push(s[i]);
+            if((i >= 0 && j < 0) || (i < 0 && j >= 0)){
+                return false;
             }
+            else if(i >= 0 && j >= 0 && s[i] != t[j]){
+                return false;
+            }
+            i--;
+            j--;
         }
-        for(int i = 0; i < t.length(); i++){
-            if(!t1.empty() && t[i] == '#'){
-                t1.pop();
-            }
-            else if(t[i] == '#'){
-                continue;
-            }
-            else{
-                t1.push(t[i]);
-            }
-        }
-        return s1 == t1;
+        return true;
     }
 };
