41595ef1a36bf6000316bc5cca52ef821dc8b050
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-12 01:26:57 +0530

    Time: 30 ms (13.12%), Space: 28.3 MB (76.45%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
     string removeStars(string s) {
+        // String will act as an active stack, dynamically grow and shrink in size as we process characters
+        // Agar stack use karte to phir se stack ko string me convert karna padta
         string ans;
         for(int i = 0; i < s.length(); i++){
+            // Agar string and empty na ho aur star bhi ho current character(to pichla character delete karna hai)
             if(!ans.empty() && s[i] == '*'){
                 ans.pop_back();
-            }
+            } 
+            // agar empty hai ya current character normal letter hai to else chalega, us character ko include kar lenge string me
+            // It joins the stack and waits to see if a future star (*) will delete it.
+            // Note: Agar stack khali ho aur tab '*' mile -> LeetCode guarantees this won't happen -> because operation is always possible(nahi to wo bhi push hoga)
             else{
                 ans.push_back(s[i]);
             }
         }
+        // This keeps the surviving characters 
+        // IN their relative order. No reversing or copying needed
         return ans;
     }
 };
+
+/*
+
+TC -> O(n) (Single pass traversal with O(1) push/pop operations | SC -> O(n) (No stars)
+
+*/
