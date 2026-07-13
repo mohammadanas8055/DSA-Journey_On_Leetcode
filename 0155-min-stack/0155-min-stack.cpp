f1f3aa9f6d3dbab9e65617e2b7b0e0c29d7f7069
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-13 19:44:55 +0530

    Time: 51 ms (58.69%), Space: 151.4 MB (27.01%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,26 +1,33 @@
 class MinStack {
 public:
-    stack<int> st;
-    stack<int> min_st;
+    stack<int> st; // Main stack- stores all values normally
+    stack<int> min_st; // Helper stack- stores minimums at each state
     MinStack() {
-        
+        // Constructor- nothing to initialize
+        // Both stacks are empty by default
     }
     
     void push(int value) {
-        st.push(value);
+        st.push(value); // Always push into main stack
+
         if(min_st.empty()){
             min_st.push(value);
-        }
-        else if(min_st.top() >= value){
+        } // First element to current minimum rahega hi 
+        else if(min_st.top() >= value){ // In case of duplicate minimum, dono baar push karna zaroori hai, warna minimum bigad jayega
+        // eg, 5, 3, 2, 2
+        //     min stack = 5, 3, 2( if only > used)
+        //     ab agar pop kara
+        // to 5, 3, 2
+        // par minimum -> 5, 3 (Minimum lost)
             min_st.push(value);
         }
     }
     
     void pop() {
-        if(min_st.top() >= st.top()){
+        if(min_st.top() == st.top()){ // pop from min_st only when jo element main stack se remove ho raha ho wo actually current minimum ho
             min_st.pop();
         }
-        st.pop();
+        st.pop(); // This should come after popping element from min_st and done always
     }
     
     int top() {
@@ -42,3 +49,11 @@ public:
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
  */
+
+/*
+
+Normal stack values store karega, aur second stack har step ka minimum store karega. Jab bhi new value current minimum se chhoti ya equal ho, usse min stack me bhi push karenge. Pop karte waqt agar popped value current minimum ke equal hai, to min stack se bhi pop karenge.
+
+TC -> O(1) in all functions, push(), pop(), top() and getMin()
+SC -> O(n)
+*/
