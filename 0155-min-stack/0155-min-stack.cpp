44ab652fdf622e37721900cd2b5890cd620fe512
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-13 17:17:37 +0530

    Time: 56 ms (48.06%), Space: 151.2 MB (45.21%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,30 +1,35 @@
 class MinStack {
 public:
-    stack<int> main_st;
+    stack<int> st;
     stack<int> min_st;
     MinStack() {
         
     }
     
     void push(int value) {
-        main_st.push(value);
-        if(min_st.empty() || value <= min_st.top()){
+        st.push(value);
+        if(min_st.empty()){
+            min_st.push(value);
+        }
+        else if(min_st.top() >= value){
             min_st.push(value);
         }
     }
     
     void pop() {
-        if(main_st.top() == min_st.top()){
+        if(min_st.top() >= st.top()){
             min_st.pop();
         }
-        main_st.pop();
+        st.pop();
     }
     
     int top() {
-        return main_st.top();
+        return st.top();
     }
     
     int getMin() {
+        // Using a loop for getting minimum -> O(n) TC 
+        // Par hame O(1) TC chahiye har function ke liye
         return min_st.top();
     }
 };
