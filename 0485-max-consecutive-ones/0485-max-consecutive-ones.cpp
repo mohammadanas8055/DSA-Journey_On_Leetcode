e0c910bbb535a1b6a727f0832521e4b7a47dab30
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-15 18:32:23 +0530

    Time: 0 ms (100%), Space: 50.2 MB (68.09%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -6,14 +6,27 @@ public:
         for(int i = 0; i < nums.size(); i++){
             if(nums[i] == 1){
                 count++;
-                if(count > maxCount){
+                if(count > maxCount){ // Updates count only when the streak grows
                     maxCount = count;
                 }
             }
             else{
                 count = 0;
             }
+            
         }
         return maxCount;
     }
 };
+
+/*
+
+Time complexity -> O(n)
+Space complexity -> O(1)
+
+Consecutive ones ko add kiya, jaise hi 0 mila to usko break kar diya, agar usse zyada kahin 1 mil gaye to us pichle wale count ko replace kar diya
+
+Could also write 
+maxCount = max(count, maxCount);
+
+*/
