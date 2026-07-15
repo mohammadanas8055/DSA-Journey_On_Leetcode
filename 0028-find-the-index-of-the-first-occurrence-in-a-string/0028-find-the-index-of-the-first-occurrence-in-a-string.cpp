6d3aaab388fe1ad2444f04ab02d7b8e8c2bb3fd5
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-15 15:08:10 +0530

    Time: 0 ms (100%), Space: 9 MB (73.29%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -14,23 +14,38 @@ public:
         int m = haystack.length();
         int n = needle.length();
 
+        // Agar needle badi hai to wo haystick me mil hi nahi sakti
         if(n > m){
             return -1;
         }
 
+        // i = haystack me starting sliding position
+        // ham m - n tak hi jayenge, kyunki uske baad needle haystack me fit nahi hogi
         for(int i = 0; i <= m - n; i++){
-            int j = 0;
+            int j = 0; // har baar needle ke shuru se hi compare karna hai na
             
+            // Needle ke har character ko match karke dekho haystack ke characters se, starting from i
             while(j < n){
                 if(haystack[i + j] != needle[j]){
-                    break;
+                    break; // agar mismatch mil gaya, loop se bahar niklo
                 }
                 j++;
             }
+
+            // agar j, n ke equal pahunch gaya, matlab saare character match ho gaye
             if(j == n){
                 return i;
             }
         }
-        return -1;
+        return -1; // kahin match nahi mila
     }
 };
+
+/*
+
+TC -> O(m * n) | SC -> O(1)
+
+This is the standard brute force approach
+Using KMP algorithm, it can be done faster
+
+*/
