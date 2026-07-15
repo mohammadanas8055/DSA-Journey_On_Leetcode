f9da3ac56d4ea87967939f3ec758f3ee88c5d7cb
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-15 16:49:01 +0530

    Time: 0 ms (100%), Space: 15 MB (87.41%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,24 +1,16 @@
-class Solution {
+class Solution{
 public:
-    bool repeatedSubstringPattern(string s) {
-        int n = s.length();
-        for(int i = 0; i < n / 2; i++){
-            int len = i + 1;
-            if(n % len != 0){
-                continue;
-            }
-            bool match = true;
-            for(int j = len; j < n; j++){
-                if(s[j] != s[j % len]){
-                    match = false;
-                    break;
-                }
-            }
-            if(match == true){
-                return true;
-            }
-        }
-        return false;
+    bool repeatedSubstringPattern(string s){
+
+        // s ko s ke sath jodo
+        string doubled = s + s;
+
+        // first aur last character hata kar substring nikalo
+        // substr(position, length)
+        string modified = doubled.substr(1, doubled.length() - 2);
+
+        // check karo agar original 's' modified string mein hai ya nahi
+        return modified.find(s) != string::npos;
     }
 };
 
@@ -40,5 +32,43 @@ public:
 // So:
 // n % len == 0 hona chahiye
 
+// TC -> O(n^2) | SC -> O(1)
 
+/*
 
+String Concatenation Trick
+Agar koi string apne aap ko repeat karke bani hai, to agar tum us string ko usi ke saath jod do (concatenate), aur starting & ending characters hata do, to bhi original string uske andar chhipi hui mil jayegi.
+
+s ko khud ke saath add karo: string doubled = s + s;
+Nayi string ka first aur last character hata do.
+Ab is bachi hui string mein check karo ki kya original s mil rahi hai ya nahi.
+Agar mil gayi -> true
+Nahi mili -> false
+
+Why does this work? (The Math/Logic)
+Maan lo s do blocks se bani hai: s = A + A.
+Jab tum s + s karte ho, to wo ban jata hai: A + A + A + A.
+
+Jab tumne first aur last char hataya, to tumne actually first aur last A block ko tod diya/kharab kar diya.
+Lekin beech ke do A blocks abhi bhi safe hain! (A_broken + A + A + A_broken).
+Aur A + A hi to tumhara original s tha!
+
+Lekin agar string repeating blocks se nahi bani, to ye middle overlapping kabhi original string nahi bana payegi.
+
+class Solution(){
+public:
+    bool repeatedSubstringPattern(string s){
+
+        // s ko s ke sath jodo
+        string doubled = s + s;
+
+        // first aur last character hata kar substring nikalo
+        // substr(position, length)
+        string modified = doubled.substr(1, doubled.length() - 2);
+
+        // check karo agar original 's' modified string mein hai ya nahi
+        return modified.find(s) != string::npos;
+    }
+};
+
+*/
