b05ce973b512eb31c80c913ce21f7371763f55f4
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-15 14:26:08 +0530

    Time: 0 ms (100%), Space: 9.1 MB (73.29%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,8 +1,35 @@
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         if(haystack.find(needle) != string::npos){
+//             return haystack.find(needle);
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        if(haystack.find(needle) != string::npos){
-            return haystack.find(needle);
+        int m = haystack.length();
+        int n = needle.length();
+
+        if(n > m){
+            return -1;
+        }
+
+        for(int i = 0; i <= m - n; i++){
+            int j = 0;
+            
+            while(j < n){
+                if(haystack[i + j] != needle[j]){
+                    break;
+                }
+                j++;
+            }
+            if(j == n){
+                return i;
+            }
         }
         return -1;
     }
