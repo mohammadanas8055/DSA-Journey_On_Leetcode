773782d04ecafdd46831650acabc78d69f94f0f0
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-16 20:13:51 +0530

    Time: 3 ms (12.67%), Space: 8.3 MB (84.94%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,41 +1,41 @@
-class Solution {
-public:
-    string makeGood(string s) {
-        int write = 0;
-        int n = s.length();
-        for(int i = 0; i < n; i++){
-            if(write == 0){
-                s[write] = s[i];
-                write++;
-            }
-            else if(s[i] != s[write - 1] && tolower(s[i]) == tolower(s[write - 1])){
-                write--;
-            }
-            else{
-                s[write] = s[i];
-                write++;
-            }
-        }
-        s.resize(write);
-        return s; 
-    }
-};
-
 // class Solution {
 // public:
 //     string makeGood(string s) {
-//         string good;
-//         for(int i = 0; i < s.length(); i++){
-//             if(good.empty()){
-//                 good.push_back(s[i]);
+//         int write = 0;
+//         int n = s.length();
+//         for(int i = 0; i < n; i++){
+//             if(write == 0){
+//                 s[write] = s[i];
+//                 write++;
 //             }
-//             else if(s[i] != good.back() && tolower(s[i]) == tolower(good.back())){
-//                 good.pop_back();
+//             else if(s[i] != s[write - 1] && tolower(s[i]) == tolower(s[write - 1])){
+//                 write--;
 //             }
 //             else{
-//                 good.push_back(s[i]);
+//                 s[write] = s[i];
+//                 write++;
 //             }
 //         }
-//         return good;
+//         s.resize(write);
+//         return s; 
 //     }
 // };
+
+class Solution {
+public:
+    string makeGood(string s) {
+        string good;
+        for(int i = 0; i < s.length(); i++){
+            if(good.empty()){
+                good.push_back(s[i]);
+            }
+            else if(s[i] != good.back() && tolower(s[i]) == tolower(good.back())){
+                good.pop_back();
+            }
+            else{
+                good.push_back(s[i]);
+            }
+        }
+        return good;
+    }
+};
