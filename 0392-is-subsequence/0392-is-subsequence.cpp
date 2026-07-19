300870757e7b63a18b2c8bb6d3eb712274dba5a3
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-19 13:10:22 +0530

    Time: 0 ms (100%), Space: 8.5 MB (67.86%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -3,7 +3,7 @@ public:
     bool isSubsequence(string s, string t) {
         int i = 0;
         for(int j = 0; j < t.length(); j++){
-            if(!s.empty() && t[j] == s[i]){
+            if(i < s.length() && t[j] == s[i]){
                i++;
             }
         }
