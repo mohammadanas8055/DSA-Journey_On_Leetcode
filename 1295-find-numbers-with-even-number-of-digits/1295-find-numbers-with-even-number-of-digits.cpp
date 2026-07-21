29e1ba6a00198b70a8dbe7cd5948934f2cce3de9
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-21 19:14:19 +0530

    Time: 0 ms (100%), Space: 13.3 MB (66.38%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
@@ -2,15 +2,11 @@ class Solution {
 public:
     int findNumbers(vector<int>& nums) {
         int count = 0;
-        // if declaring here,  digits never get reset and gets acculmulating across numbers
-        // isliye loop ke andar rakhna hoga digits = 0
-        // taki har number ke liye 0 se digits count hon
         for(int i = 0; i < nums.size(); i++){
-            int n = nums[i];
             int digits = 0;
-            while(n > 0){
+            while(nums[i] != 0){
                 digits++;
-                n = n / 10;
+                nums[i] /= 10;
             }
             if(digits % 2 == 0){
                 count++;
@@ -19,18 +15,3 @@ public:
         return count;
     }
 };
-
-/*
-
-this is TC O(n) only and not O(n^2)
-because the inner while loop is not running n times for every element
-it runs according to the number of digits in that number
-to O(n*d) ho jayegi complexity , where d is from 1 to 10^5
-to d constant 
-to O(n) hi ho gayi TC 
-
-aur SC O(1)  
-
-Small general case note: if n = 0, this gives digits = 0, but digit count of 0 should be 1. LeetCode 1295 has positive numbers, so it’s fine.
-
-*/
