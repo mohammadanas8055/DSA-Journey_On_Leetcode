9d98029944e7dd3d1f30d78f390ac38857339019
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-24 03:04:32 +0530

    Time: 0 ms (100%), Space: 10.8 MB (41.85%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -1,12 +1,33 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        int add = 0;
-        int maxHeight= 0;
+        int altitude = 0; // altitude current gain add karta jayega
+                          // Starting altitude question ke according 0 hai
+
+        int maxAltitude = 0; // highest altitude bhi initially 0 se start hoga
+                             // kyunki starting point khud ek valid altitude hai
+                             // Agar saare gains negative hue, tab bhi answer 0 ho sakta hai
+
         for(int i = 0; i < gain.size(); i++){
-            add = add + gain[i];
-            maxHeight = max(maxHeight, add);
+            altitude = altitude + gain[i]; // current gain ko revious altitude me add karke
+                                           // next/current altitude nikaal li
+
+            maxAltitude = max(maxAltitude, altitude); // har step pe check kar rahe hain ki
+                                                      // nayi altitude ab tak ki sabse badi altitiude hai ya nahi
+                                                      // Agar badi hai to answer update kar do
         }
-        return maxHeight;
+        return maxAltitude; // puri trip me mili hui highest altitude return kar di
     }
 };
+
+/*
+
+shuru me altitude 0 hai, isliye maxAltitude = 0 se shuru kiya
+phir jo gain ho raha hai use add karte jayen, maximum mile(0 se bada) to usko update kar lenge
+
+TC -> O(n) | SC -> O(1)
+
+Pattern: 
+running sum + track maximum
+
+*/
