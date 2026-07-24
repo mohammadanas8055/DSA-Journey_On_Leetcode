1078d5510ff7be06a1aa33d32b2b88184f7e8212
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-24 15:03:00 +0530

    Time: 5 ms (39.33%), Space: 21.9 MB (92.82%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -17,11 +17,8 @@ public:
         int i = 0;
         int j = s.length() - 1;
         while(i < j){
-            if(s[i] != s[j] && isPalindrome(s, i + 1, j) || s[i] != s[j] && isPalindrome(s, i, j - 1)){
-                return true;               
-            }
             if(s[i] != s[j]){
-                return false;
+                return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
             }
             i++;
             j--;
