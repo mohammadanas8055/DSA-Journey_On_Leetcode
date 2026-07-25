86c992c2e210bed4d5f3e35034b98d4622a1dcf3
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-25 23:56:26 +0530

    Time: 0 ms (100%), Space: 11.2 MB (83.38%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -1,32 +1,14 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        int maxWealth = 0;
-        for(int i = 0; i < accounts.size(); i++){ // This goes customer by customer, i.e., row by row
-            int wealth = 0; // Har ek customer ke liye, wealth ko reset karna hoga
-        for(int j = 0; j < accounts[i].size();j++){ // Goes through all bank accounts for that customer
-                wealth = wealth + accounts[i][j];
+        int maximumWealth = 0;
+        for(int i = 0; i < accounts.size(); i++){
+            int currentWealth = 0;
+            for(int j = 0; j < accounts[i].size(); j++){
+                currentWealth += accounts[i][j];
             }
-            if(wealth > maxWealth){
-                maxWealth = wealth;
-            }
+            maximumWealth = max(maximumWealth, currentWealth);
         }
-        return maxWealth;
+        return maximumWealth;
     }
 };
-
-/*
-
-accounts is a 2D vector
-So, accounts.size() returns number of rows
-one row = one customer
-
-accounts[i].size() returns number of elements in row i
-
-Therefore i goes over customers/rows
-          j goes over banks for that customer/columns
-          
-TC -> O(no. of rows * no. of columns) or O(total number of elements)
-SC -> O(1)
-
-*/
