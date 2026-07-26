bc6959e1340c61083fd5ba9f335a24805630d071
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-26 07:44:41 +0530

    Time: 0 ms (100%), Space: 11.8 MB (39.67%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        stack<int> st;
-        for(int i = sandwiches.size() - 1; i >= 0; i--){
-            st.push(sandwiches[i]);
+        vector<int> count(2, 0);
+        for (int i = 0; i < students.size(); i++) {
+            if (students[i] == 0) {
+                count[0]++;
+            } else {
+                count[1]++;
+            }
         }
-        queue<int> q;
-        for(int i = 0; i < students.size(); i++){
-            q.push(students[i]);
-        }
-        int rotation = 0;
-        while(!st.empty()){
-            if(q.front() != st.top()){
-                int temp = q.front();
-                q.pop();
-                q.push(temp);
-                rotation++;
-                if(rotation == q.size()){
-                    break;
-                }
+        for (int i = 0; i < sandwiches.size(); i++) {
+            if(sandwiches[i] == 0 && count[0] != 0){
+                count[0]--;
+            }
+            else if(sandwiches[i] == 1 && count[1] != 0){
+                count[1]--;
             }
             else{
-                q.pop();
-                st.pop();
-                rotation = 0;
+                break;
             }
         }
-        return q.size();
+        int answer = 0;
+        for(int i = 0; i < 2; i++){
+            answer += count[i];
+        }
+        return answer;    
     }
 };
