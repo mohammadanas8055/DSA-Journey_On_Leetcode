d87ecb788c8e4876a3e2af96b53dc350c8ff5cff
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-26 23:58:28 +0530

    Time: 0 ms (100%), Space: 10.1 MB (99.56%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -16,11 +16,32 @@ public:
             while(i < j && !isVowel(s[j])){
                 j--;
             }
+            if(i < j){
                 swap(s[i], s[j]);
                 i++;
                 j--;
-            
+            }
         }
         return s;
     }
 };
+
+/*
+
+Main problem is: 
+vowels ko reverse karo
+So directly ye hi decide karna hai current character vowel hai ya nahi
+
+So, 
+Step 1: left pointer ko tab tak move karo jab tak vowel na mil jaye
+Step 2: right pointer ko tab tak move karo jab tak vowel na mil jaye
+Step 3: Agar dono vowels mil gaye aur i < j, then swap
+Step 4: Swap ke baad pointers move
+
+Therefore "if" nahi, while skip loops use karo(with the help of helper function)
+
+Also same iteration me pointers multiple times mpove honge to check karna padega(ki i < j hai bhi ya nahi, nahi to agar vowel mila hi nahi, to infinite loop ban jayega)
+
+
+
+*/
