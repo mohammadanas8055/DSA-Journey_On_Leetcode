49351634b0d9ee46409692956c9e72e3f63a447d
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-07-28 22:18:03 +0530

    Time: 0 ms (100%), Space: 12.6 MB (69.56%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.cpp
+++ 0832-flipping-an-image/0832-flipping-an-image.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
         for(int i = 0; i < image.size(); i++){
             int left = 0; 
-            int right = image[i].size() - 1;
+            int right = image.size() - 1;
             while(left <= right){
                 int temp = 1 - image[i][left];
                 image[i][left] = 1 - image[i][right];
@@ -11,20 +11,7 @@ public:
                 left++;
                 right--;
             }
-            // invert and flip at the same time(left <= right is needed so that the middle element also gets inverted) OR
-            // for(int j = 0; j < image[i].size(); j++){
-            //     image[i][j] = 1 - image[i][j];
-            // }
         }
         return image;
     }
 };
-
-/*
-
-TC -> O(n^2) or O(rows*cols) | SC -> O(1)
-
-. Pattern used:
-2D vector traversal + row-wise two pointer + bit inversion
-
-*/
