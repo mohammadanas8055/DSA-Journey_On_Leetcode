745b30bed21bc8a78213acd6d70a7bdc43cbe294
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-08-03 23:59:14 +0530

    Time: 0 ms (100%), Space: 13.5 MB (19.91%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> result(2 * n);
-        int pos = 0;
-        int i = 0;
-        int j = n;
-        while(j < 2 * n){
-            result[pos] = nums[i];
-            i++;
-            pos++;
-            result[pos] = nums[j];
-            j++;
-            pos++;
+        vector<int> result(nums.size()); 
+        // Final answer array banayi
+        // nums.size() = 2 * n, so result ka size bhi utna hi hoga
+
+        for(int i = 0; i < n; i++){
+            // Loop sirf na times chalega
+            // Kyunki total n pairs hain: (x1,y1), (x2,y2), (x3,y3)....(xn,yn)
+
+            result[2 * i] = nums[i]; 
+            // x wala(first half ka) element hamesha even index pe aayega
+
+            result[2 * i + 1] = nums[n + i]; 
+            // y wala(Second half ka) element hamesha odd index pe aayega
+            // Why n+i -> second half index n se start hota hai 
         }
+        // There are n pairs and i am placing two elements(one pair) for each iteration
+        // That's why loop n times(n pairs getting placed)
         return result;
+        // Shuffled array return kar di
     }
 };
+
+/*
+
+TC -> O(2n) -> O(n) | SC -> O(2n) -> O(n)
+
+*/
+
