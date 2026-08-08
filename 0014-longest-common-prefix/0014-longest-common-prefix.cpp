09a4d7f43daf3a0276e022c988bbcd163df1219c
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-08-08 13:16:56 +0530

    Time: 0 ms (100%), Space: 11.7 MB (93.76%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -7,11 +7,25 @@ public:
                     continue;
                 }
                 else{
-                    strs[0].resize(j);
-                    return strs[0];
+                    return strs[0].substr(0, j);
                 }
             }
         }
         return strs[0];
     }
 };
+
+/*
+
+How Would I Place A Pointer At Each Word...If There are 200 Words? -> Ans. We don't need 200 pointers
+Instead we manage two things:
+Which character index are we looking at?(j)
+2. Which word are we comparing it against(i)
+
+So let's look the firsrt word, and assume it might be the common prefix
+Ab uske ek ek character ko check karo, dusre words ke first character se(loop from 1 to strs.size() - 1)
+Agar aage ka koi word chhota na ho, aur phir match bhi kar gaya, to next character pe shift
+agar first word khatam ho gaya, iska matlab wahi maximum common prefix ho sakta hai(Aur saare word usse chhote the length me)
+Jaise hi mismatch mile, waise hi stop kar dena hai
+
+*/
