f0f8fa5d3063a23fa87baab98eefd161ea29eb04
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-08-11 17:42:10 +0530

    Time: 0 ms (100%), Space: 25.9 MB (50.41%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
@@ -3,17 +3,19 @@ public:
     vector<int> sortArrayByParityII(vector<int>& nums) {
         int even = 0;
         int odd = 1;
-        vector<int> ans(nums.size());
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] % 2 == 0){
-                ans[even] = nums[i];
+        while(even <= nums.size() - 1 && odd <= nums.size() - 1){
+            if(nums[even] % 2 == 0){
                 even += 2;
+                continue;
             }
-            else{
-                ans[odd] = nums[i];
+            if(nums[odd] % 2 != 0){
                 odd += 2;
+                continue;
             }
+            swap(nums[odd], nums[even]);
+            odd += 2;
+            even += 2;
         }
-        return ans;
+        return nums;
     }
 };
