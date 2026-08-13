5a46207225dc298e4bfb1a47f8e3219caa3ff350
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-08-13 16:46:25 +0530

    Time: 0 ms (100%), Space: 12.4 MB (94.07%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -1,43 +1,43 @@
-// class Solution {
-// public:
-//     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-//         vector<bool> maxCandies(candies.size(), true);
-//         for(int i = 0; i < candies.size(); i++){
-//             int temp = candies[i] + extraCandies;
-//             for(int j = 0; j < candies.size(); j++){
-//                 if(temp < candies[j]){
-//                     maxCandies[i] = false;
-//                 }
-//             }
-//         }
-//         return maxCandies;
-//     }
-// };
-
-// This is O(n^2) TC
-// For every kid, we are searching whether he has more candies than the maximum one
-// Instead, find the maximum one and then compare each kid with that maximum value
-
-class Solution{
+class Solution {
 public:
-    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies){
-        int maximum = candies[0];
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> yesOrNo(candies.size());
+        // Answer vector pre-allocate ki hai candies size ki
+        // Har index par true/false store hoga
+
+        int greatest = candies[0];
+        // greatest me current maximum candies count store hoga
+        // Initial value pehle bacche ki candies se start ki
+        // Kyunki kisi bhi single element se comparison shuru karna safe hai
+
         for(int i = 1; i < candies.size(); i++){
-            if(candies[i] > maximum){
-                maximum = candies[i];
-            }
+            greatest = max(greatest, candies[i]);
+            // Loop 1 se start kiya kyunki index 0 already greatest me hai
+            // Har baar current value ko current maximum se compare kiye
         }
-        vector<bool> result;
+        // Loop ke baad greatest me sabse zyada candies wala value hoga
+        
         for(int i = 0; i < candies.size(); i++){
-            if(candies[i] + extraCandies >= maximum){
-                result.push_back(true);
+            if(extraCandies + candies[i] >= greatest){
+                yesOrNo[i] = true;
+                // Agar current bacche ki candies+extraCandies
+                // greatest ke barabar ya usse zyada ho jaye
+                // to wo bacha "greatest" ban sakta hai
+
+                // >= isliye kyunki barabar hone par bhi bacha tie kar leta hai
             }
             else{
-                result.push_back(false);
+                yesOrNo[i] = false;
+                // Warna wo greatest tak nahi pahunch sakta
             }
         }
-        return result;
+        return yesOrNo;
+        // Har bacche ke liye true/false ka vector return kar diya
     }
 };
 
-// TC -> O(n) + O(n) = O(n) | SC -> O(n)
+/*
+
+TC -> O(n) + O(n) = O(n) | SC -> O(n) (Extra auxiliary O(1))
+
+*/
