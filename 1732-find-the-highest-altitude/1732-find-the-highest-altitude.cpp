595b47ae64beb833cf8979219da95e4865c4b542
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-08-15 11:59:47 +0530

    Time: 0 ms (100%), Space: 10.9 MB (41.86%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        int altitude = 0; // altitude current gain add karta jayega
-                          // Starting altitude question ke according 0 hai
+        int altitude = 0; 
+        // altitude current gain add karta jayega
+        // Starting altitude question ke according 0 hai
 
-        int maxAltitude = 0; // highest altitude bhi initially 0 se start hoga
-                             // kyunki starting point khud ek valid altitude hai
-                             // Agar saare gains negative hue, tab bhi answer 0 ho sakta hai
+        int maxAltitude = 0; 
+        // highest altitude bhi initially 0 se start hoga
+        // kyunki starting point khud ek valid altitude hai
+        // Agar saare gains negative hue, tab bhi answer 0 ho sakta hai
 
         for(int i = 0; i < gain.size(); i++){
-            altitude = altitude + gain[i]; // current gain ko revious altitude me add karke
-                                           // next/current altitude nikaal li
+            altitude += gain[i]; 
+            // current gain ko revious altitude me add karke
+            // next/current altitude nikaal li
 
-            maxAltitude = max(maxAltitude, altitude); // har step pe check kar rahe hain ki
-                                                      // nayi altitude ab tak ki sabse badi altitiude hai ya nahi
-                                                      // Agar badi hai to answer update kar do
+            maxAltitude = max(maxAltitude, altitude); 
+            // har step pe check kar rahe hain ki
+            // nayi altitude ab tak ki sabse badi altitiude hai ya nahi
+            // Agar badi hai to answer update kar do
         }
-        return maxAltitude; // puri trip me mili hui highest altitude return kar di
+        return maxAltitude;
+        // puri trip me mili hui highest altitude return kar di
     }
 };
 
