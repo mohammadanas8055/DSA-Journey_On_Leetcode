c8c1939c55b0222b695afde064f4435716a4af48
mohammadanas8055 <mohammadanas0078600@gmail.com>
2026-09-05 18:37:09 +0530

    Time: 0 ms (100%), Space: 7.8 MB (61.44%) - LeetHub

--- 0504-base-7/0504-base-7.cpp
+++ 0504-base-7/0504-base-7.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     string convertToBase7(int num) {
-        string s;
-        int sign = 1;
         if(num == 0){
             return "0";
         }
+        string s;
+        int sign = 1;
         if(num < 0){
             num = num * -1;
             sign = -1;
@@ -14,15 +14,10 @@ public:
             s.push_back((num % 7) + '0');
             num = num / 7;
         }
-        reverse(s.begin(), s.end());
         if(sign == -1){
-            string temp = s;
-            s.clear();
             s.push_back('-');
-            for(int i = 0; i < temp.length(); i++){
-                s.push_back(temp[i]);
-            }
         }
+        reverse(s.begin(), s.end());
         return s;
     }
 };
